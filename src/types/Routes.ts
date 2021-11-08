cd736f2051145723ca7a74898135554323f1a55c
v.berezin <v.berezin@corp.mail.ru>
2021-11-08 16:51:10 +0300

    Routes.ts

---
+++ src/types/Routes.ts
@@ -0,0 +1,12 @@
+export enum Routes {
+  index = '/',
+  game = '/game',
+  forum = '/forum',
+  profile = '/profile',
+  profileEdit = '/profile/edit',
+  profilePassword = '/profile/password',
+  signIn = '/signin',
+  signOut = '/signout',
+  signUp = '/signup',
+  leaderBoards = '/leaderboards',
+}
