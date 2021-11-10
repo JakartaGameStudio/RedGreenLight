8fb1ebed7b63f99f22ae3ab46c40bc82e39d3a5c
v.berezin <v.berezin@corp.mail.ru>
2021-11-10 03:00:18 +0300

    [breakpoints]

--- src/components/Breakpoints/Breakpoints.tsx
+++ src/components/Breakpoints/Breakpoints.tsx
@@ -4,8 +4,10 @@ import { useBreakpoints } from './useBreakpoints';
 export function Breakpoints({ points, children }: BreakpointsProps) {
   const [current] = useBreakpoints();
   const active = [].concat(points).some((point) => current.includes(point));
+
   if (!active) {
     return null;
   }
+
   return <>{children}</>;
 }
--- src/components/Breakpoints/useBreakpoints.ts
+++ src/components/Breakpoints/useBreakpoints.ts
@@ -6,22 +6,22 @@ export function useBreakpoints() {
   const [current, setCurrent] = useState([]);
 
   function filter(mediaList) {
-    return mediaList.reduce((result, { name, media }) => {
-      if (media.matches) {
-        return [...result, name];
-      }
-      return result;
-    }, []);
+    return mediaList.filter(({ media }) => media.matches).map(({ name }) => name);
   }
 
   useEffect(() => {
     const mediaList = Object.entries(points).map(([name, screen]) => {
       const media = window.matchMedia(`(min-width: ${screen})`);
       media.onchange = () => setCurrent(filter(mediaList));
+
       return { name, media };
     });
     setCurrent(filter(mediaList));
+
+    return function () {
+      mediaList.forEach(({ media }) => (media.onchange = null));
+    };
   }, []);
 
-  return [current];
+  return current;
 }
