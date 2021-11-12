cd6b0f6659d1ba4622c905dd0170d755021f5718
dinkagafon <agafonikita@outlook.com>
2021-11-12 17:56:46 +0300

    [leaderboards] refactor style

--- src/components/LeaderboardsBlock/LeaderboardsBlock.tsx
+++ src/components/LeaderboardsBlock/LeaderboardsBlock.tsx
@@ -1,23 +1,24 @@
 import classNames from 'classnames';
-
+import UserIcon from 'images/icons/user.svg';
+import { LeaderboarsPlayer } from 'types/LeaderboarsPlayer.types';
 
 import styles from './LeaderboardsBlock.module.scss';
-import { LeaderboarsPlayer } from 'types/LeaderboarsPlayer.types';
-import UserIcon from 'images/icons/user.svg';
 
 export function LeaderboardsBlock({ login, place, value, isOwn }: LeaderboarsPlayer) {
   return (
-    <div className={classNames(styles.block, {
-      [styles.ownBlock]: isOwn,
-    })} >
-      <div className={styles.place}>{place}</div>
-      <div className={styles.playerInfo}>
-        <div className={styles.icon}>
+    <div
+      className={classNames(styles.leaderboardsBlock, {
+        [styles.leaderboardsBlock_ownBlock]: isOwn,
+      })}
+    >
+      <div className={styles.leaderboardsBlock__place}>{place}</div>
+      <div className={styles.leaderboardsBlock__info}>
+        <div className={styles.leaderboardsBlock__icon}>
           <UserIcon width="1em" height="1em" />
         </div>
-        <p className={styles.login}>{login}</p>
+        <p className={styles.leaderboardsBlock__login}>{login}</p>
       </div>
-      <div className={styles.time}>{value}</div>
+      <div className={styles.leaderboardsBlock__time}>{value}</div>
     </div>
   );
 }
