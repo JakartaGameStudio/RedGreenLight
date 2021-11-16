21a59e97678c3392c18813fb0bc9d46c0adff66d
dinkagafon <agafonikita@outlook.com>
2021-11-16 17:15:13 +0300

    [lificycle_game]

--- src/containers/GameContainer/useGame.ts
+++ src/containers/GameContainer/useGame.ts
@@ -8,8 +8,8 @@ export const useGame = (config) => {
   const canvasRef = useRef(null);
   const [renderTime, setRenderTime] = useState('');
   const [gameStatus, setGameStatus] = useState<keyof typeof GameStatus>(GameStatus.beforeGame);
-  const gameElements = useMemo(() => {
-    const mainHero = new MainHero({
+  const { mainHero, timeController, finishLine, game, borderGame } = useMemo(() => {
+    const _mainHero = new MainHero({
       x: config.HERO_START_POSITION_X,
       y: config.HERO_START_POSITION_Y,
       radius: config.HERO_DEFAULT_RADIUS,
@@ -24,43 +24,43 @@ export const useGame = (config) => {
         fill: config.HERO_FILL_LOSE_COLOR,
       },
     });
-    const timeController = new TimeController({
+    const _timeController = new TimeController({
       timeout: 2000,
       safePeriod: 500,
       allowedMoveTime: 3000,
       totalTime: config.MAX_GAME_TIME,
     });
-    const borderGame = new BorderGame({
+    const _borderGame = new BorderGame({
       width: config.GAME_WIDTH,
       height: config.GAME_HEIGHT,
     });
-    const game = new Game();
-    const finishLine = new FinishLine({
+    const _game = new Game();
+    const _finishLine = new FinishLine({
       height: config.GAME_HEIGHT,
       lengthDistance: config.GAME_FIELD_DISTANCE_LENGTH,
     });
 
     return {
-      mainHero,
-      timeController,
-      borderGame,
-      game,
-      finishLine,
+      mainHero: _mainHero,
+      timeController: _timeController,
+      borderGame: _borderGame,
+      game: _game,
+      finishLine: _finishLine,
     };
   }, []);
   const gameActions = useMemo(() => {
     return {
       startGame: () => {
-        gameElements.game.start();
+        game.start();
       },
       restartGame: () => {
-        gameElements.game.restart();
+        game.restart();
       },
       startBoost: () => {
-        gameElements.mainHero.startBoost();
+        mainHero.startBoost();
       },
       endBoost: () => {
-        gameElements.mainHero.endBoost();
+        mainHero.endBoost();
       },
     };
   }, []);
@@ -81,84 +81,76 @@ export const useGame = (config) => {
     canvas.width = config.GAME_WIDTH;
     canvas.height = config.GAME_HEIGHT;
 
-    gameElements.game.beforeStart = () => {
+    game.beforeStart = () => {
       setGameStatus(GameStatus.inGame);
-      gameElements.timeController.start();
+      timeController.start();
     };
 
-    gameElements.game.logic = (timeFraction: number) => {
-      gameElements.timeController.updateTime(timeFraction);
+    game.logic = (timeFraction: number) => {
+      timeController.updateTime(timeFraction);
 
-      if (gameElements.timeController.allowedMove) {
-        gameElements.borderGame.color = config.GAME_BORDER_ALLOWED_COLOR;
+      if (timeController.allowedMove) {
+        borderGame.color = config.GAME_BORDER_ALLOWED_COLOR;
       }
 
-      if (gameElements.timeController.safePeriod) {
-        gameElements.borderGame.color = config.GAME_BORDER_WARNING_COLOR;
+      if (timeController.safePeriod) {
+        borderGame.color = config.GAME_BORDER_WARNING_COLOR;
       }
 
-      if (gameElements.timeController.timeout) {
-        gameElements.borderGame.color = config.GAME_BORDER_STOP_COLOR;
+      if (timeController.timeout) {
+        borderGame.color = config.GAME_BORDER_STOP_COLOR;
       }
 
-      if (gameElements.mainHero.leftBorder > gameElements.finishLine.lengthDistance) {
-        gameElements.mainHero.isWon = true;
+      if (mainHero.leftBorder > finishLine.lengthDistance) {
+        mainHero.isWon = true;
       }
 
-      if (
-        gameElements.mainHero.speed > 0 &&
-        gameElements.timeController.timeout &&
-        !gameElements.mainHero.isWon
-      ) {
-        gameElements.mainHero.isLost = true;
+      if (mainHero.speed > 0 && timeController.timeout && !mainHero.isWon) {
+        mainHero.isLost = true;
       }
 
-      if (gameElements.timeController.timeEnded && !gameElements.mainHero.isWon) {
-        gameElements.borderGame.color = config.GAME_BORDER_STOP_COLOR;
-        gameElements.mainHero.isLost = true;
+      if (timeController.timeEnded && !mainHero.isWon) {
+        borderGame.color = config.GAME_BORDER_STOP_COLOR;
+        mainHero.isLost = true;
       }
 
-      if (gameElements.mainHero.isLost) {
-        gameElements.timeController.stop();
+      if (mainHero.isLost) {
+        timeController.stop();
         setGameStatus('lose');
       }
 
-      if (gameElements.mainHero.isWon) {
-        gameElements.timeController.stop();
+      if (mainHero.isWon) {
+        timeController.stop();
         setGameStatus('win');
       }
 
-      if (
-        gameElements.timeController.timeOver &&
-        !gameElements.mainHero.isLost &&
-        !gameElements.mainHero.isWon
-      ) {
-        gameElements.timeController.reset();
+      if (timeController.timeOver && !mainHero.isLost && !mainHero.isWon) {
+        timeController.reset();
       }
 
-      gameElements.mainHero.move(timeFraction);
-      gameElements.mainHero.move(timeFraction);
+      mainHero.move(timeFraction);
+      mainHero.move(timeFraction);
     };
 
-    gameElements.game.render = () => {
-      gameElements.borderGame.render(ctx);
-      gameElements.mainHero.render(ctx);
-      gameElements.finishLine.render(ctx);
-      const tTime = new Date(gameElements.timeController.remainingTime);
+    game.render = () => {
+      borderGame.render(ctx);
+      mainHero.render(ctx);
+      finishLine.render(ctx);
+      const tTime = new Date(timeController.remainingTime);
 
       setRenderTime(`${tTime.getMinutes()}:${tTime.getSeconds()}`);
     };
 
-    gameElements.game.clear = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.clear = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    gameElements.game.restart = () => {
-      gameElements.mainHero.refresh();
-      gameElements.timeController.refresh();
+    game.restart = () => {
+      mainHero.refresh();
+      timeController.refresh();
       setGameStatus('inGame');
     };
 
     return () => {
-      gameElements.game.clearAnimate();
+      game.clearAnimate();
     };
   }, []);
 
