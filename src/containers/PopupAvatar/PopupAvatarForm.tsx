ea1bf6c1f31515f5aa189decae3ad24396ef8e81
v.berezin <v.berezin@corp.mail.ru>
2021-11-18 00:20:55 +0300

    [profile]

--- src/containers/PopupAvatar/PopupAvatarForm.tsx
+++ src/containers/PopupAvatar/PopupAvatarForm.tsx
@@ -27,17 +27,23 @@ export function PopupAvatarForm({ onSubmit, onClose }: PopupAvatarFormProps) {
   return (
     <form className={styles.form} onSubmit={handlerSubmit}>
       <label className={styles.label}>
-        {!value && <span className={styles.link}>Выбрать файл на компьютере</span>}
-        {value && <img src={URL.createObjectURL(value)} alt="Аватар" className={styles.image} />}
-        {value && <span className={styles.value}>{value.name}</span>}
+        {value && (
+          <>
+            <img src={URL.createObjectURL(value)} alt="Аватар" className={styles.image} />
+            <span className={styles.value}>{value.name}</span>
+          </>
+        )}
+        <span className={styles.link}>
+          {value ? 'Выбрать другой файл' : 'Выбрать файл на компьютере'}
+        </span>
         <input type="file" className={styles.input} accept=".png,.jpg,.svg" onChange={onChange} />
       </label>
       {value && (
         <Button type="submit" className={styles.button}>
-          Поменять
+          Применить
         </Button>
       )}
-      <Button type="reset" className={styles.button} mod="light" onClick={onClose}>
+      <Button type="reset" className={styles.button} mod="warning-light" onClick={onClose}>
         Отмена
       </Button>
     </form>
