4e29ed37fc43fd7e371884f0e6daeefd2de2657a
dinkagafon <agafonikita@outlook.com>
2021-11-25 18:17:26 +0300

    [sw] add sw

--- static/sw.js
+++ static/sw.js
@@ -23,51 +23,18 @@ this.addEventListener("install", event => {
 
 this.addEventListener('fetch', event => { 
   event.respondWith( 
-      // Пытаемся найти ответ на такой запрос в кеше 
-      caches.match(event.request) 
-          .then(response => { 
-              // Если ответ найден, выдаём его 
-              if (response) { 
-                  return response; 
-              } 
-
-      const fetchRequest = event.request.clone(); 
-      // В противном случае делаем запрос на сервер 
-      return fetch(fetchRequest) 
-        // Можно задавать дополнительные параметры запроса, если ответ вернулся некорректный. 
-      .then(response => { 
-          // Если что-то пошло не так, выдаём в основной поток результат, но не кладём его в кеш 
-          if(!response || response.status !== 200 || response.type !== 'basic') { 
-            return response; 
-          } 
-
-          const responseToCache = response.clone(); 
-          // Получаем доступ к кешу по CACHE_NAME 
-          caches.open(CACHE_NAME) 
-          .then(cache => { 
-              // Записываем в кеш ответ, используя в качестве ключа запрос 
-              cache.put(event.request, responseToCache); 
-          }); 
-          // Отдаём в основной поток ответ 
-          return response; 
-       } 
-      ); 
-  }) 
-); 
+    fetch(event.request).catch(() => {
+      return caches.match(event.request)
+    })
+  ); 
 }); 
 
-this.addEventListener("activate", event => {
-  console.log("activate");
-}); 
-
-this.addEventListener("activate", event => { 
+this.addEventListener("activate", function(event) { 
   event.waitUntil( 
-      caches.keys().then(cacheNames => { 
-          return Promise.all( 
-              cacheNames 
-              .filter(name => {/* Нужно вернуть true, если хотите удалить этот файл из кеша совсем */}) 
-      .map(name => caches.delete(name))  
-    ) 
-  })
-); 
-});
+    caches.keys().then(cacheNames => { 
+      return Promise.all( 
+          cacheNames.map((name) => caches.delete(name))  
+      ) 
+    })
+  )
+})
