f209d282d56bf5c41fc925eb9d2c7741dcd15062
Никита Агафонов <dinkagafon>
2022-01-11 20:22:48 +0300

    [ssr] first build

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,5 +1,6 @@
 import './App.module.scss';
 
+import { ErrorBoundary } from 'components/ErrorBoundary/ErrorBoundary';
 import { ProtectedRoute } from 'components/ProtectedRoute/ProtectedRoute';
 import { Page404 } from 'pages/Page404/Page404';
 import { PageAuth } from 'pages/PageAuth/PageAuth';
@@ -9,27 +10,30 @@ import { PageIndex } from 'pages/PageIndex/PageIndex';
 import { PageLeaderBoards } from 'pages/PageLeaderBoards/PageLeaderBoards';
 import { PageProfile } from 'pages/PageProfile/PageProfile';
 import { PageSignOut } from 'pages/PageSignOut/PageSignOut';
+import { hot } from 'react-hot-loader/root';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppRoutes } from 'types/AppRoutes';
 
-export function App() {
+export const App = hot(() => {
   return (
-    <Router>
-      <Routes>
-        <Route path={AppRoutes.index} element={<PageIndex />} />
-        <Route path={AppRoutes.game} element={<PageGame />} />
-        <Route path={AppRoutes.forum} element={<PageForum />} />
-        <Route path={AppRoutes.signIn} element={<PageAuth />} />
-        <Route path={AppRoutes.signUp} element={<PageAuth signUp={true} />} />
-        <Route path={AppRoutes.signOut} element={<PageSignOut />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path={AppRoutes.profile} element={<PageProfile />} />
-          <Route path={AppRoutes.profileEdit} element={<PageProfile type="edit" />} />
-          <Route path={AppRoutes.profilePassword} element={<PageProfile type="password" />} />
-          <Route path={AppRoutes.leaderBoards} element={<PageLeaderBoards />} />
-        </Route>
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <Routes>
+          <Route path={AppRoutes.index} element={<PageIndex />} />
+          <Route path={AppRoutes.game} element={<PageGame />} />
+          <Route path={AppRoutes.forum} element={<PageForum />} />
+          <Route path={AppRoutes.signIn} element={<PageAuth />} />
+          <Route path={AppRoutes.signUp} element={<PageAuth signUp={true} />} />
+          <Route path={AppRoutes.signOut} element={<PageSignOut />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path={AppRoutes.profile} element={<PageProfile />} />
+            <Route path={AppRoutes.profileEdit} element={<PageProfile type="edit" />} />
+            <Route path={AppRoutes.profilePassword} element={<PageProfile type="password" />} />
+            <Route path={AppRoutes.leaderBoards} element={<PageLeaderBoards />} />
+          </Route>
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
-}
+});
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+/*import 'babel-polyfill';
+
+import express, { RequestHandler } from 'express';
+import path from 'path';
+import webpack from 'webpack';
+import devMiddleware from 'webpack-dev-middleware';
+import hotMiddleware from 'webpack-hot-middleware';
+
+import config from '../webpack/client.config';
+import serverRenderMiddleware from './server-render-middleware';
+
+// Эта функция возвращает middleware для локального девсервера и HMR
+// Она должна работать только для режима разработки
+function getWebpackMiddlewares(config: webpack.Configuration): RequestHandler[] {
+  const compiler = webpack({ ...config, mode: 'development' });
+
+  return [
+    // Middleware для Webpack-билда проекта в реальном времени. Низкоуровневый аналог webpack-dev-server
+    devMiddleware(compiler, {
+      logLevel: 'error',
+      publicPath: config.output!.publicPath!,
+    }),
+    // Middleware для HMR
+    hotMiddleware(compiler, { path: `/__webpack_hmr` }),
+  ];
+}
+
+const app = express();
+
+// Отдаём статику приложения
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// На все get запросы запускаем сначала middleware dev server, а потом middleware рендеринга приложения
+app.get('/*', [...getWebpackMiddlewares(config)], serverRenderMiddleware);
+
+export { app };*/
