abb126a6b80f8218a4c573b4d6719a453e8610be
v.berezin <v.berezin@corp.mail.ru>
2022-02-05 23:17:01 +0300

    remove тестовый console.log

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
 const client = require('./config/client/webpack.config.client');
 const server = require('./config/server/webpack.config.server');
-console.log('ENV VARS:', process.env);
+
 module.exports = [client, server];
